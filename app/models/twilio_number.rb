1d568b426743f086948d45e98ec89010ce72c211
Eric Hankins <ehankins@rednovalabs.com>
2012-11-11 01:04:42 -0600

    Upgrade logging

--- app/models/twilio_number.rb
+++ app/models/twilio_number.rb
@@ -8,7 +8,7 @@ class TwilioNumber < ActiveRecord::Base
     end
     
     truncated_message = message[0, 160]
-    Rails.logger.ap("****** Sending Twilio message to #{user.nickname} (#{user.phone_number}), from (#{phone}): #{truncated_message}")
+    Rails.logger.info("****** Sending Twilio message to #{user.nickname} (#{user.phone_number}), from (#{phone}): #{truncated_message}")
     
     if Rails.env.production? || user.phone_number == "+18162132421"
       @twilio_client = Twilio::REST::Client.new ApplicationConfig[:twilio_account], ApplicationConfig[:twilio_token]
