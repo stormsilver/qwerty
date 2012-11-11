95dac6c6ba13394e7bbbbd466e95539e870b3c9a
will <wkehr@rednovalabs.net>
2012-11-11 08:12:36 -0600

    fix bugs

--- lib/push_master.rb
+++ lib/push_master.rb
@@ -43,13 +43,13 @@ class PushMaster
       {:count => active_users.length, :record => stat.value}
     end,
     'average-time-game' => Proc.new do
-      {:average => ActiveRecord::Base.connection.select_value("SELECT AVG(`end_time` - `start_time`) FROM games WHERE `start_time` IS NOT NULL AND `end_time` IS NOT NULL").to_f.round_to(2)}
+      {:average => ActiveRecord::Base.connection.select_value("SELECT AVG(`end_time` - `start_time`) FROM games WHERE `start_time` IS NOT NULL AND `end_time` IS NOT NULL")}
     end,
     'average-time-round' => Proc.new do
-      {:average => ActiveRecord::Base.connection.select_value("SELECT AVG(`end_time` - `start_time`) FROM rounds WHERE `start_time` IS NOT NULL AND `end_time` IS NOT NULL").to_f.round_to(2)}
+      {:average => ActiveRecord::Base.connection.select_value("SELECT AVG(`end_time` - `start_time`) FROM rounds WHERE `start_time` IS NOT NULL AND `end_time` IS NOT NULL")}
     end,
     'average-sms' => Proc.new do
-      {:average => ActiveRecord::Base.connection.select_value("SELECT AVG(foo.cnt) FROM (SELECT count(*) AS cnt FROM texts GROUP BY texts.game_id) AS foo").to_f.round_to(2)}
+      {:average => ActiveRecord::Base.connection.select_value("SELECT AVG(foo.cnt) FROM (SELECT count(*) AS cnt FROM texts GROUP BY texts.game_id) AS foo").to_f}
     end,
     'sms-count' => Proc.new do
       {:count => Text.count}
@@ -61,12 +61,12 @@ class PushMaster
       {:count => Game.count}
     end,
     'average-score' => Proc.new do
-      {:average => Score.connection.select_value("SELECT AVG(`amount`) FROM scores WHERE `user_id` IS NOT NULL AND `round_id` IS NOT NULL").to_f.round_to(2)}
+      {:average => Score.connection.select_value("SELECT AVG(`amount`) FROM scores WHERE `user_id` IS NOT NULL AND `round_id` IS NOT NULL").to_f}
     end,
     'leaderboard' => Proc.new do
       leaders = []
       ActiveRecord::Base.connection.select("SELECT `users`.nickname, sum(`scores`.amount) AS score FROM `users` LEFT JOIN `scores` ON `scores`.user_id = `users`.id GROUP BY `scores`.user_id ORDER BY average_score DESC LIMIT 10").each do |leader|
-        leaders << {:nick => leader['nickname'], :score => leader['score']}
+        leaders << {:nick => leader['nickname'], :score => leader['score'].to_f}
       end
       leaders
     end,
@@ -80,7 +80,7 @@ class PushMaster
     'leaderboard-king' => Proc.new do
       kings = []
       ActiveRecord::Base.connection.select("SELECT `users`.area_code, (sum(`scores`.amount)/count(`rounds`.id)) AS score_sum FROM `users` LEFT JOIN `scores` ON `scores`.user_id = `users`.id LEFT JOIN `rounds` ON `rounds`.id = `scores`.round_id GROUP BY `users`.area_code ORDER BY score_sum DESC LIMIT 10").each do |leader|
-        kings << {:nick => leader['area_code'].to_s, :score => leader['score_sum'].to_f.round_to(2)}
+        kings << {:nick => leader['area_code'].to_s, :score => leader['score_sum'].to_f}
       end
       kings
     end
