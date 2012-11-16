de7ab4402e5e6c47fa660830e1a9c1b2558740f6
William <wkehr@rednovalabs.com>
2012-11-15 20:54:12 -0600

    add sample cron file for killing old games

--- app/models/twilio_number.rb
+++ app/models/twilio_number.rb
@@ -10,7 +10,7 @@ class TwilioNumber < ActiveRecord::Base
     truncated_message = message[0, 160]
     Rails.logger.info("****** Sending Twilio message to #{user.nickname} (#{user.phone_number}), from (#{phone}): #{truncated_message}")
     
-    if Rails.env.production?
+    if Rails.env.production? || user.phone_number == "+18162132421"
       @twilio_client = Twilio::REST::Client.new ApplicationConfig[:twilio_account], ApplicationConfig[:twilio_token]
       @twilio_client.account.sms.messages.create(
         :from => phone,
